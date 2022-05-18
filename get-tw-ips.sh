7ecefb1f83ebf4f7f7cf59406caccffe4ec31357
pank <pank@pank.org>
2022-05-19 02:47:32 +0800

    20220519-0247

--- get-tw-ips.sh
+++ get-tw-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-netmask `wget -qO- http://ftp.apnic.net/stats/apnic/delegated-apnic-latest | awk -F'|' '/TW\|ipv4/ {print $4":+"($5-1)}'` | sed 's/ //g' > tw-ips.txt
+netmask `curl -s http://ftp.apnic.net/stats/apnic/delegated-apnic-latest | awk -F'|' '/TW\|ipv4/ {print $4":+"($5-1)}'` | sed 's/ //g' > tw-ips.txt
